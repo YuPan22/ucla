6a20ce570eca2d71504cbc2118463a739d2d7e10
yupan22 <ouyeyu@gmail.com>
2020-01-23 13:34:34 -0800

    minor fix

--- waveform/tests/test_deidentifier.py
+++ waveform/tests/test_deidentifier.py
@@ -1,15 +1,15 @@
 import pytest
 
-from ..to_csv.single_file_processor import SingleFilePorcessor
+from ..to_csv.single_file_processor import SingleFileProcessor
 
 def test_deidentified_filename():
     filename1 = "_20181016185515_0000_RR.vital"
-    df1 = SingleFilePorcessor("dob","mask").deidentified_filename(filename=filename1)
+    df1 = SingleFileProcessor("dob","mask").deidentified_filename(filename=filename1)
     expected_df1 = "_185515_0000_RR.vital"
     assert df1 == expected_df1
 
     filename2 = "_20190718144723_20190718144727.adibin"
-    df2 = SingleFilePorcessor("dob","mask").deidentified_filename(filename=filename2)
+    df2 = SingleFileProcessor("dob","mask").deidentified_filename(filename=filename2)
     expected_df2 = "_144723_144727.adibin"
     assert df2 == expected_df2
 
